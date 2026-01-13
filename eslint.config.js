7a71f5999fd5b430b70d984d0f99603cab4aef5e
julietmayer <julitamajer@gmail.com>
2026-01-13 15:55:11 +0100

    eslint config and pretty-quick

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,23 @@
 // @ts-check
 const eslint = require('@eslint/js');
-const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
 
-module.exports = defineConfig([
+module.exports = tseslint.config(
   {
     files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      prettierConfig,
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     processor: angular.processInlineTemplates,
     rules: {
       '@angular-eslint/directive-selector': [
@@ -31,11 +36,13 @@ module.exports = defineConfig([
           style: 'kebab-case',
         },
       ],
+      'prettier/prettier': 'error', // Enforce Prettier formatting as lint errors
+      '@typescript-eslint/no-explicit-any': 'error', // Forbid usage of 'any'
     },
   },
   {
     files: ['**/*.html'],
-    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
     rules: {},
   },
-]);
+);
